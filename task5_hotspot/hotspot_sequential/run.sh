f918664d7c3c334fdbbc0bbc4fb69ec38657b3c0
mdodovic <matija.dodovic@gmail.com>
2022-04-17 13:11:29 +0200

    add sequential code

---
+++ task5_hotspot/hotspot_sequential/run.sh
@@ -0,0 +1,6 @@
+./hotspot 32 32 8192 1 ./data/temp32_32 ./data/power32_32 ./output/out32_32
+./hotspot 256 256 8192 1 ./data/temp256_256 ./data/power256_256 ./output/out256_256
+./hotspot 1024 1024 4096 1 ./data/temp1024_1024 ./data/power1024_1024 ./output/out1024_1024_4096
+./hotspot 1024 1024 8192 1 ./data/temp1024_1024 ./data/power1024_1024 ./output/out1024_1024_8192
+./hotspot 1024 1024 16384 1 ./data/temp1024_1024 ./data/power1024_1024 ./output/out1024_1024_16384
+./hotspot 1024 1024 32768 1 ./data/temp1024_1024 ./data/power1024_1024 ./output/out1024_1024_32768
